1e014ac3695c2ba76beb2b4e78378b636bf4f20b
n1kko777 <3734489@gmail.com>
2020-03-12 20:51:37 +0500

    Тестирование

--- app/tests/core/test_views.py
+++ app/tests/core/test_views.py
@@ -8,149 +8,6 @@ from django.test import Client
 pytestmark = [pytest.mark.django_db]
 
 
-def tests_Tag_list_view():
-    instance1 = test_helpers.create_core_Tag()
-    instance2 = test_helpers.create_core_Tag()
-    client = Client()
-    url = reverse("core_Tag_list")
-    response = client.get(url)
-    assert response.status_code == 200
-    assert str(instance1) in response.content.decode("utf-8")
-    assert str(instance2) in response.content.decode("utf-8")
-
-
-def tests_Tag_create_view():
-    profile = test_helpers.create_core_Profile()
-    client = Client()
-    url = reverse("core_Tag_create")
-    data = {
-        "tag_color": "text",
-        "tag_name": "text",
-        "profile": profile.pk,
-    }
-    response = client.post(url, data)
-    assert response.status_code == 302
-
-
-def tests_Tag_detail_view():
-    client = Client()
-    instance = test_helpers.create_core_Tag()
-    url = reverse("core_Tag_detail", args=[instance.pk, ])
-    response = client.get(url)
-    assert response.status_code == 200
-    assert str(instance) in response.content.decode("utf-8")
-
-
-def tests_Tag_update_view():
-    profile = test_helpers.create_core_Profile()
-    client = Client()
-    instance = test_helpers.create_core_Tag()
-    url = reverse("core_Tag_update", args=[instance.pk, ])
-    data = {
-        "tag_color": "text",
-        "tag_name": "text",
-        "profile": profile.pk,
-    }
-    response = client.post(url, data)
-    assert response.status_code == 302
-
-
-def tests_Action_list_view():
-    instance1 = test_helpers.create_core_Action()
-    instance2 = test_helpers.create_core_Action()
-    client = Client()
-    url = reverse("core_Action_list")
-    response = client.get(url)
-    assert response.status_code == 200
-    assert str(instance1) in response.content.decode("utf-8")
-    assert str(instance2) in response.content.decode("utf-8")
-
-
-def tests_Action_create_view():
-    account = test_helpers.create_core_Account()
-    tags = test_helpers.create_core_Tag()
-    client = Client()
-    url = reverse("core_Action_create")
-    data = {
-        "action_amount": 1.0,
-        "account": account.pk,
-        "tags": tags.pk,
-    }
-    response = client.post(url, data)
-    assert response.status_code == 302
-
-
-def tests_Action_detail_view():
-    client = Client()
-    instance = test_helpers.create_core_Action()
-    url = reverse("core_Action_detail", args=[instance.pk, ])
-    response = client.get(url)
-    assert response.status_code == 200
-    assert str(instance) in response.content.decode("utf-8")
-
-
-def tests_Action_update_view():
-    account = test_helpers.create_core_Account()
-    tags = test_helpers.create_core_Tag()
-    client = Client()
-    instance = test_helpers.create_core_Action()
-    url = reverse("core_Action_update", args=[instance.pk, ])
-    data = {
-        "action_amount": 1.0,
-        "account": account.pk,
-        "tags": tags.pk,
-    }
-    response = client.post(url, data)
-    assert response.status_code == 302
-
-
-def tests_Category_list_view():
-    instance1 = test_helpers.create_core_Category()
-    instance2 = test_helpers.create_core_Category()
-    client = Client()
-    url = reverse("core_Category_list")
-    response = client.get(url)
-    assert response.status_code == 200
-    assert str(instance1) in response.content.decode("utf-8")
-    assert str(instance2) in response.content.decode("utf-8")
-
-
-def tests_Category_create_view():
-    profile = test_helpers.create_core_Profile()
-    client = Client()
-    url = reverse("core_Category_create")
-    data = {
-        "category_color": "text",
-        "caterory_name": "text",
-        "profile": profile.pk,
-    }
-    response = client.post(url, data)
-    assert response.status_code == 302
-
-
-def tests_Category_detail_view():
-    client = Client()
-    instance = test_helpers.create_core_Category()
-    url = reverse("core_Category_detail", args=[instance.pk, ])
-    response = client.get(url)
-    assert response.status_code == 200
-    assert str(instance) in response.content.decode("utf-8")
-
-
-def tests_Category_update_view():
-    profile = test_helpers.create_core_Profile()
-    client = Client()
-    instance = test_helpers.create_core_Category()
-    url = reverse("core_Category_update", args=[instance.pk, ])
-    data = {
-        "category_color": "text",
-        "caterory_name": "text",
-        "profile": profile.pk,
-    }
-    response = client.post(url, data)
-    assert response.status_code == 302
-
-
 def tests_Transfer_list_view():
     instance1 = test_helpers.create_core_Transfer()
     instance2 = test_helpers.create_core_Transfer()
@@ -200,107 +57,95 @@ def tests_Transfer_update_view():
     assert response.status_code == 302
 
 
-def tests_Profile_list_view():
-    instance1 = test_helpers.create_core_Profile()
-    instance2 = test_helpers.create_core_Profile()
+def tests_Category_list_view():
+    instance1 = test_helpers.create_core_Category()
+    instance2 = test_helpers.create_core_Category()
     client = Client()
-    url = reverse("core_Profile_list")
+    url = reverse("core_Category_list")
     response = client.get(url)
     assert response.status_code == 200
     assert str(instance1) in response.content.decode("utf-8")
     assert str(instance2) in response.content.decode("utf-8")
 
 
-def tests_Profile_create_view():
-    user = test_helpers.create_users_CustomUser()
+def tests_Category_create_view():
+    company = test_helpers.create_core_Company()
     client = Client()
-    url = reverse("core_Profile_create")
+    url = reverse("core_Category_create")
     data = {
-        "email_confirmed": True,
-        "phone_confirmed": True,
-        "last_name": "text",
-        "first_name": "text",
-        "phone": "text",
-        "image": "anImage",
-        "email": "user@tempurl.com",
-        "user": user.pk,
+        "caterory_name": "text",
+        "category_color": "text",
+        "company": company.pk,
     }
     response = client.post(url, data)
     assert response.status_code == 302
 
 
-def tests_Profile_detail_view():
+def tests_Category_detail_view():
     client = Client()
-    instance = test_helpers.create_core_Profile()
-    url = reverse("core_Profile_detail", args=[instance.pk, ])
+    instance = test_helpers.create_core_Category()
+    url = reverse("core_Category_detail", args=[instance.pk, ])
     response = client.get(url)
     assert response.status_code == 200
     assert str(instance) in response.content.decode("utf-8")
 
 
-def tests_Profile_update_view():
-    user = test_helpers.create_users_CustomUser()
+def tests_Category_update_view():
+    company = test_helpers.create_core_Company()
     client = Client()
-    instance = test_helpers.create_core_Profile()
-    url = reverse("core_Profile_update", args=[instance.pk, ])
+    instance = test_helpers.create_core_Category()
+    url = reverse("core_Category_update", args=[instance.pk, ])
     data = {
-        "email_confirmed": True,
-        "phone_confirmed": True,
-        "last_name": "text",
-        "first_name": "text",
-        "phone": "text",
-        "image": "anImage",
-        "email": "user@tempurl.com",
-        "user": user.pk,
+        "caterory_name": "text",
+        "category_color": "text",
+        "company": company.pk,
     }
     response = client.post(url, data)
     assert response.status_code == 302
 
 
-def tests_Account_list_view():
-    instance1 = test_helpers.create_core_Account()
-    instance2 = test_helpers.create_core_Account()
+def tests_Tag_list_view():
+    instance1 = test_helpers.create_core_Tag()
+    instance2 = test_helpers.create_core_Tag()
     client = Client()
-    url = reverse("core_Account_list")
+    url = reverse("core_Tag_list")
     response = client.get(url)
     assert response.status_code == 200
     assert str(instance1) in response.content.decode("utf-8")
     assert str(instance2) in response.content.decode("utf-8")
 
 
-def tests_Account_create_view():
-    profile = test_helpers.create_core_Profile()
+def tests_Tag_create_view():
+    company = test_helpers.create_core_Company()
     client = Client()
-    url = reverse("core_Account_create")
+    url = reverse("core_Tag_create")
     data = {
-        "balance": 1.0,
-        "account_name": "text",
-        "account_color": "text",
-        "profile": profile.pk,
+        "tag_name": "text",
+        "tag_color": "text",
+        "company": company.pk,
     }
     response = client.post(url, data)
     assert response.status_code == 302
 
 
-def tests_Account_detail_view():
+def tests_Tag_detail_view():
     client = Client()
-    instance = test_helpers.create_core_Account()
-    url = reverse("core_Account_detail", args=[instance.pk, ])
+    instance = test_helpers.create_core_Tag()
+    url = reverse("core_Tag_detail", args=[instance.pk, ])
     response = client.get(url)
     assert response.status_code == 200
     assert str(instance) in response.content.decode("utf-8")
 
 
-def tests_Account_update_view():
-    profile = test_helpers.create_core_Profile()
+def tests_Tag_update_view():
+    company = test_helpers.create_core_Company()
     client = Client()
-    instance = test_helpers.create_core_Account()
-    url = reverse("core_Account_update", args=[instance.pk, ])
+    instance = test_helpers.create_core_Tag()
+    url = reverse("core_Tag_update", args=[instance.pk, ])
     data = {
-        "balance": 1.0,
-        "account_name": "text",
-        "account_color": "text",
-        "profile": profile.pk,
+        "tag_name": "text",
+        "tag_color": "text",
+        "company": company.pk,
     }
     response = client.post(url, data)
     assert response.status_code == 302
@@ -347,6 +192,116 @@ def tests_Company_update_view():
     assert response.status_code == 302
 
 
+def tests_Profile_list_view():
+    instance1 = test_helpers.create_core_Profile()
+    instance2 = test_helpers.create_core_Profile()
+    client = Client()
+    url = reverse("core_Profile_list")
+    response = client.get(url)
+    assert response.status_code == 200
+    assert str(instance1) in response.content.decode("utf-8")
+    assert str(instance2) in response.content.decode("utf-8")
+
+
+def tests_Profile_create_view():
+    user = test_helpers.create_User()
+    company = test_helpers.create_core_Company()
+    client = Client()
+    url = reverse("core_Profile_create")
+    data = {
+        "phone": "text",
+        "image": "anImage",
+        "first_name": "text",
+        "is_admin": True,
+        "last_name": "text",
+        "company_identificator": "text",
+        "phone_confirmed": True,
+        "user": user.pk,
+        "company": company.pk,
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+
+
+def tests_Profile_detail_view():
+    client = Client()
+    instance = test_helpers.create_core_Profile()
+    url = reverse("core_Profile_detail", args=[instance.pk, ])
+    response = client.get(url)
+    assert response.status_code == 200
+    assert str(instance) in response.content.decode("utf-8")
+
+
+def tests_Profile_update_view():
+    user = test_helpers.create_User()
+    company = test_helpers.create_core_Company()
+    client = Client()
+    instance = test_helpers.create_core_Profile()
+    url = reverse("core_Profile_update", args=[instance.pk, ])
+    data = {
+        "phone": "text",
+        "image": "anImage",
+        "first_name": "text",
+        "is_admin": True,
+        "last_name": "text",
+        "company_identificator": "text",
+        "phone_confirmed": True,
+        "user": user.pk,
+        "company": company.pk,
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+
+
+def tests_Account_list_view():
+    instance1 = test_helpers.create_core_Account()
+    instance2 = test_helpers.create_core_Account()
+    client = Client()
+    url = reverse("core_Account_list")
+    response = client.get(url)
+    assert response.status_code == 200
+    assert str(instance1) in response.content.decode("utf-8")
+    assert str(instance2) in response.content.decode("utf-8")
+
+
+def tests_Account_create_view():
+    profile = test_helpers.create_core_Profile()
+    client = Client()
+    url = reverse("core_Account_create")
+    data = {
+        "account_color": "text",
+        "account_name": "text",
+        "balance": 1.0,
+        "profile": profile.pk,
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+
+
+def tests_Account_detail_view():
+    client = Client()
+    instance = test_helpers.create_core_Account()
+    url = reverse("core_Account_detail", args=[instance.pk, ])
+    response = client.get(url)
+    assert response.status_code == 200
+    assert str(instance) in response.content.decode("utf-8")
+
+
+def tests_Account_update_view():
+    profile = test_helpers.create_core_Profile()
+    client = Client()
+    instance = test_helpers.create_core_Account()
+    url = reverse("core_Account_update", args=[instance.pk, ])
+    data = {
+        "account_color": "text",
+        "account_name": "text",
+        "balance": 1.0,
+        "profile": profile.pk,
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+
+
 def tests_Transaction_list_view():
     instance1 = test_helpers.create_core_Transaction()
     instance2 = test_helpers.create_core_Transaction()
@@ -359,8 +314,8 @@ def tests_Transaction_list_view():
 
 
 def tests_Transaction_create_view():
-    tags = test_helpers.create_core_Tag()
-    account = test_helpers.create_core_Account()
+    tags = test_helpers.create_Tag()
+    account = test_helpers.create_Account()
     category = test_helpers.create_core_Category()
     client = Client()
     url = reverse("core_Transaction_create")
@@ -384,8 +339,8 @@ def tests_Transaction_detail_view():
 
 
 def tests_Transaction_update_view():
-    tags = test_helpers.create_core_Tag()
-    account = test_helpers.create_core_Account()
+    tags = test_helpers.create_Tag()
+    account = test_helpers.create_Account()
     category = test_helpers.create_core_Category()
     client = Client()
     instance = test_helpers.create_core_Transaction()
@@ -395,6 +350,55 @@ def tests_Transaction_update_view():
         "tags": tags.pk,
         "account": account.pk,
         "category": category.pk,
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+
+
+def tests_Action_list_view():
+    instance1 = test_helpers.create_core_Action()
+    instance2 = test_helpers.create_core_Action()
+    client = Client()
+    url = reverse("core_Action_list")
+    response = client.get(url)
+    assert response.status_code == 200
+    assert str(instance1) in response.content.decode("utf-8")
+    assert str(instance2) in response.content.decode("utf-8")
+
+
+def tests_Action_create_view():
+    tags = test_helpers.create_Tag()
+    account = test_helpers.create_core_Account()
+    client = Client()
+    url = reverse("core_Action_create")
+    data = {
+        "action_amount": 1.0,
+        "tags": tags.pk,
+        "account": account.pk,
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+
+
+def tests_Action_detail_view():
+    client = Client()
+    instance = test_helpers.create_core_Action()
+    url = reverse("core_Action_detail", args=[instance.pk, ])
+    response = client.get(url)
+    assert response.status_code == 200
+    assert str(instance) in response.content.decode("utf-8")
+
+
+def tests_Action_update_view():
+    tags = test_helpers.create_Tag()
+    account = test_helpers.create_core_Account()
+    client = Client()
+    instance = test_helpers.create_core_Action()
+    url = reverse("core_Action_update", args=[instance.pk, ])
+    data = {
+        "action_amount": 1.0,
+        "tags": tags.pk,
+        "account": account.pk,
     }
     response = client.post(url, data)
     assert response.status_code == 302
