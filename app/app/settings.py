83faf892c534841b19f0e2caa94cce561bfc8f1c
n1kko777 <3734489@gmail.com>
2020-03-13 13:56:08 +0500

    Static Files

--- app/app/settings.py
+++ app/app/settings.py
@@ -126,13 +126,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = './vol/web/media'
+STATIC_ROOT = './vol/web/static'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
