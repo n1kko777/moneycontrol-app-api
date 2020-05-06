2660f5646a2fff8baf624b08b031529d52eee24d
n1kko777 <3734489@gmail.com>
2020-05-06 14:07:17 +0500

    ###

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(",")
