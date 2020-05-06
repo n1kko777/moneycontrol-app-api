b004c2f5f0c33f18eaa750ece937f9ef4621caec
n1kko777 <3734489@gmail.com>
2020-05-06 10:46:41 +0500

    Настройка авторизации

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_auth.registration.views import VerifyEmailView, RegisterView
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_swagger_view(title='MoneyControl API')
@@ -12,11 +13,19 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    # enables reset_password, you can see reset email in logs
+    path('rest-auth/registration/',
+         RegisterView.as_view(), name='account_signup'),
+    re_path(r'^rest-auth/registration/account-confirm-email/',
+            VerifyEmailView.as_view(),
+            name='account_email_verification_sent'),
+    re_path(
+        r'^rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$',
+        VerifyEmailView.as_view(),
+        name='account_confirm_email'),
     path('', include('django.contrib.auth.urls')),
     path('api/v1/', include('core.urls', namespace='api')),
     path('', schema_view),
-    path('api-auth/', include('rest_framework.urls', \
+    path('api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
 ]
 
