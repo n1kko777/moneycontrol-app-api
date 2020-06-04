4b5d2a5696f766a191ebe0bf384866d2284f468c
n1kko777 <3734489@gmail.com>
2020-06-04 16:39:33 +0500

    Переезд

--- bin/start_gunicorn.sh
+++ bin/start_gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /home/www/code/moneycontrol-app-api/env/bin/activate
-source /home/www/code/moneycontrol-app-api/env/bin/postactivate
-exec gunicorn  -c "/home/www/code/moneycontrol-app-api/gunicorn_config.py" app.wsgi
+source /home/n1kko777/code/moneycontrol-app-api/env/bin/activate
+source /home/n1kko777/code/moneycontrol-app-api/env/bin/postactivate
+exec gunicorn  -c "/home/n1kko777/code/moneycontrol-app-api/gunicorn_config.py" app.wsgi
