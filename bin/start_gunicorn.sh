3e742cda7d28221ea557f4b099f3bc56ce1103e4
n1kko777 <3734489@gmail.com>
2020-06-08 08:17:17 +0500

    Fix gunicorn

--- bin/start_gunicorn.sh
+++ bin/start_gunicorn.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source /home/n1kko777/code/moneycontrol-app-api/env/bin/activate
-source /home/n1kko777/code/moneycontrol-app-api/env/bin/postactivate
 exec gunicorn  -c "/home/n1kko777/code/moneycontrol-app-api/gunicorn_config.py" app.wsgi
--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 command = '/home/n1kko777/code/moneycontrol-app-api/env/bin/gunicorn'
 pythonpath = '/home/n1kko777/code/moneycontrol-app-api/app'
 bind = '127.0.0.1:8001'
-workers = 3
+workers = 5
 user = 'n1kko777'
 limit_request_fields = 32000
 limit_request_field_size = 0
