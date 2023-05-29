00569adb6a1fba9e4215d5f7cd4bc1df79117ed5
Тарасов Никита Александрович <NAlTarasov@sberbank.ru>
2023-05-29 21:17:52 +0500

    Change static

--- app/settings.py
+++ app/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
